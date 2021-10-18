31d648f54cafa65419a0bbed53dc90d03b91f8bd
BraveHyena <furkan_fb_26214441@hotmail.com>
2021-10-19 00:22:28 +0300

    18 ekim ders sonu.

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Selam Js")
+// JavaScript'te Type Safe Yok.
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+// ARRAY 
+// İsimlendirme olarak camelCasing kullanılır.
+
+let konutKredileri = 
+["Konut Kredisi",
+"Emlak Konut Kredisi",
+"Kamu Konut Kredisi",
+"Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>" +konutKredileri[i]+ "</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
